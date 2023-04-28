2e5146a14bf1d46a76a23f4f4643029d928bbf68
Fernando Cabral <fernandoantoniojp10@gmail.com>
2023-04-28 14:16:58 -0300

    Adicionando projeto basico do curso sobre dictionary

---
+++ Course/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+                        //dictionary.Add(name, votes);
+
+                        if (dictionary.ContainsKey(name))
+                        {
+                            dictionary[name] += votes;
+                        }
+                        else
+                        {
+                            dictionary[name] = votes;
+                        }
+                    }
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine(dictionary.Keys + ": " + dictionary.Count);
+                    }
+                    
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
